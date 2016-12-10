f01c033e12d4ca16152d84d3b9b5b85e7497e908
Yong Li <yong.b.li@intel.com>
2016-12-10 11:16:31 +0800

    eth0 okay after modify kernel command: clk_ignore_unused
    Signed-off-by: Yong Li <yong.b.li@intel.com>

--- mk.sh
+++ mk.sh
@@ -5,6 +5,7 @@ time make LOADADDR=0x30008000  ARCH=arm CROSS_COMPILE="ccache arm-linux-gnueabih
 
 cat arch/arm/boot/zImage arch/arm/boot/dts/s5pv210-smdkv210.dtb > zImage
 
+#dtc -I dtb arch/arm/boot/dts/s5pv210-smdkv210.dtb > all.dts
 
 #console=ttySAC2,115200 root=/dev/nfs nfsroot=10.239.53.6:/home/yongli/rootfs-arm ip=10.239.53.113:10.239.53.113:10.239.53.6:255.255.255.0:x210:eth0:off ipv6.disable=1 rootwait debug
 
@@ -13,7 +14,7 @@ cat arch/arm/boot/zImage arch/arm/boot/dts/s5pv210-smdkv210.dtb > zImage
 #root=/dev/ram0 rw ramdisk=8192 initrd=0x20800000,8M console=ttySAC1,115200 init=/linuxrc
 
 #boot using full initramfs
-#console=ttySAC2,115200 debug earlyprintk=ttySAC2,115200
+#setenv bootargs 'console=ttySAC2,115200 debug earlyprintk=ttySAC2,115200 clk_ignore_unused'
 
 # boot using mini initramfs, no /init
 # root=/dev/ram rw console=ttySAC2,115200 rdinit=/bin/sh debug earlyprintk=ttySAC2,115200
